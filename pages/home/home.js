8301c2da0ff8a9b9762b08e2d5aa39078af654dd
wzw-github <you@163.com>
2019-11-24 15:51:00 +0800

    网络请求

--- pages/home/home.js
+++ pages/home/home.js
@@ -11,56 +11,100 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
+    // 1发送网络请求
 
+
+    // get请求，没参数
+    // wx.request({
+    //   url: 'http://localhost:8080/testPage/WeiXin',
+    //   success:function(res){
+    //     console.log(res)
+    //   }
+    // })
+
+    // get请求，有参数
+    // wx.request({
+    //   url: 'http://localhost:8080/testPage/WeiXin?num=1',
+    //   success: function (res) {
+    //     console.log(res)
+    //   }
+    // })
+
+    // get请求，有参数
+    // wx.request({
+    //   url: 'http://localhost:8080/testPage/WeiXin',
+    //   data:{
+    //     num:1
+    //   },
+    //   success: function (res) {
+    //     console.log(res)
+    //   }
+    // })
+
+    // post请求，有参数
+    wx.request({
+      // url: 'http://httpbin.org/post',
+      url: 'http://localhost:8080/testPage/WeiXin',
+      method:'post',
+      data: {
+        num:1
+      },
+      success: function(a) {
+        console.log(a)
+      },
+      fail:function(a){
+
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
